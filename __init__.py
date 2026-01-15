9a509684fa3d5d80aea3b7790938b8ff8d58ded7
Ben Orr <bendorr2415@gmail.com>
2026-01-15 12:04:28 -0800

    update init

--- __init__.py
+++ __init__.py
@@ -2,12 +2,18 @@
 lucs_af
 
 LUCS (Loop-Helix-Loop Unit Combinatorial Sampling) structural analysis 
-and AlphaFold2 fine-tuning.
+and AlphaFold2 fine-tuning and inference.
 
-Includes:
-- AlphaFold fine-tuning and evaluation
-- Structural analysis for Rosetta models and predicted structures
-- Helix geometry and loop-helix-loop reshaping analysis
+LUCS structural analysis:
+- Calculating protein structural diversity and similarity (RMSD, TM-score, helix RMSD, helix displacement and angle)
+- Analyzing and visualizing helix geometry/diversity, e.g., the extent to which LUCS helices were reshaped from their starting positions
+- Analyzing predicted structures' confidence metrics (pLDDT, pAE, pTM), structure-based metrics, and Rosetta metrics
+
+AlphaFold fine-tuning:
+- Fine-tuning AlphaFold2 models on custom datasets
+- Fine-tuning classifier heads on top of AlphaFold2 for protein property prediction
+- Running classic and fine-tuned AlphaFold2 predictions with and without MSA and templates
+- Evaluating predictions for structural and classification accuracy
 
 Author: Ben Orr
 """
